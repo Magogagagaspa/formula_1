cade30c7985793bdc52e610d62626a3ca3a74bdd
Isabel654321 <isabel.varisco@liceobanfi.eu>
2023-05-15 11:22:49 +0200

    sistemato footer

--- script_header_footer/script_footer.js
+++ script_header_footer/script_footer.js
@@ -1,7 +1,33 @@
 document.write(`   <footer>
-<div class="foot">
-  <p>federico.sala01@liceobanfi.eu  -  gasparing.davide@liceobanfi.eu</p>
-  <p>+39 346 6433 325  -  +39 542 2655 886</p>
-  <p>Sala Federico  -  Gasparin Davide</p>
-</div>
+<div class="footer-container"> 
+      <img src="immagini/f11.png" alt="" >
+      
+         <div class="footer-container-social">
+          
+          <a href="https://twitter.com/f1"><img src="immagini/social/twittter.png" alt="" ></a>
+          <a href="https://www.facebook.com/Formula1/"><img src="immagini/social/facebook.png" alt="" ></a>
+          <a href="https://www.instagram.com/f1/"><img src="immagini/social/instagrammm.png" alt="" ></a>
+          <a href="https://www.youtube.com/user/Formula1"><img src="immagini/social/youtube.png" alt="" ></a>
+         </div>
+      <div class="footer-container-partners">
+          <p>I nostri partner</p>
+          <ul>
+              <a href="https://www.rolex.com/it"><li><img src="https://media.formula1.com/image/upload/content/dam/fom-website/2020/sponsors/rolex.png" alt="" ></li></a>
+              <a href="https://www.pirelli.com/global/it-it/homepage"><li><img src="https://media.formula1.com/image/upload/content/dam/fom-website/2020/sponsors/pirelli.png" alt="" ></li></a>
+              <a href="https://www.aramco.com/"><li><img src="https://media.formula1.com/image/upload/content/dam/fom-website/2020/sponsors/aramco.png" alt="" ></li></a>
+              <a href="https://www.heineken.com/it/it/storia"><li><img src="https://media.formula1.com/image/upload/content/dam/fom-website/2020/sponsors/Heineken%20-%20Outlined.png" alt="" ></li></a>
+              <a href="https://www.dhl.com/it-it/home.html"><li><img src="https://media.formula1.com/image/upload/content/dam/fom-website/2020/sponsors/dhl.png" alt="" ></li></a>
+              <a href="https://www.qatarairways.com/"><li><img src="https://media.formula1.com/image/upload/content/dam/fom-website/2020/sponsors/qatar.png" alt="" id="margin"></li></a>
+              <a href="https://aws.amazon.com/"><li><img src="https://media.formula1.com/image/upload/content/dam/fom-website/2020/sponsors/AWS%20GLOBAL.png" alt="" ></li></a>
+          </ul>
+          <ul class="footer-container-partners-it">
+              <li><a href="https://www.paramountplus.com/"><img src="https://media.formula1.com/image/upload/f_auto/q_auto/v1681231879/fom-website/2020/sponsors/Paramount%2B.png" alt="" id="margin"></a></li>
+              <li><a href="https://www.lenovo.com/"><img src="https://media.formula1.com/image/upload/content/dam/fom-website/2020/sponsors/lenovo.png" alt=""></a></li>
+              <li><a href="https://www.liqui-moly.com/"><img src="https://media.formula1.com/image/upload/content/dam/fom-website/2020/sponsors/liqui-moly.png" alt="" ></a></li>
+  
+          </ul>
+          <div class="footer-copyright">
+            
+            <h4>© 2003-2023 Formula One World Championship Limited</h4>
+            
 </footer>`)
